677c51b4e0b12cf15a6a3f0fa8f9b453e7d98ca4
hftamayo <hftamayo@gmail.com>
2022-12-12 20:02:11 -0600

    nft collection smart contract

---
+++ 2022/02sophomore/02nft_collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xd0376534caEFe18F930b222E6F1bB1Dc8e83a143";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
